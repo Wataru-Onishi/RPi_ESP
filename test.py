712b4994b11fd70864859c9670807b21b3318744
Wataru Onishi <62089701+Wataru-Onishi@users.noreply.github.com>
2024-03-02 17:04:03 +0900

    Update test.py

--- test.py
+++ test.py
@@ -20,10 +20,10 @@ dxl_packetHandler = PacketHandler(PROTOCOL_VERSION)
 
 # Base speed settings for each motor
 dxl_base_speeds = {
-    1: 200,  # Base speed for motor ID 1
-    2: 200,  # Base speed for motor ID 2
-    3: 200,  # Base speed for motor ID 3
-    4: 200   # Base speed for motor ID 4
+    1: 100,  # Base speed for motor ID 1
+    2: 100,  # Base speed for motor ID 2
+    3: 100,  # Base speed for motor ID 3
+    4: 100   # Base speed for motor ID 4
 }
 
 # Open the Dynamixel port
