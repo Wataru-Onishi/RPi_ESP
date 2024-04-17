28ce3a753b2a0a8c0ac87b4f4af90bf1a69e352d
Wataru Onishi <62089701+Wataru-Onishi@users.noreply.github.com>
2024-04-17 13:20:44 +0900

    Update torque_test.py

--- torque_test.py
+++ torque_test.py
@@ -1,27 +1,25 @@
-import os
 from dynamixel_sdk import *                    # Uses Dynamixel SDK library
-
 # Control table address
-ADDR_OPERATING_MODE    = 11                    # Operating mode address
-ADDR_GOAL_CURRENT      = 102                   # Goal current address
-ADDR_TORQUE_ENABLE     = 64                    # Torque enable address
-ADDR_HARDWARE_ERROR    = 70                    # Hardware error status
+ADDR_PRO_CURRENT_LIMIT = 38                    # EEPROM area to limit peak current
+ADDR_PRO_GOAL_CURRENT = 102                    # Address of Goal Current
+ADDR_PRO_TORQUE_ENABLE = 64                    # Address of Torque Enable
+ADDR_PRO_OPERATING_MODE = 11                   # Operating Mode Address
 
 # Data Byte Length
-LEN_GOAL_CURRENT       = 2
+LEN_PRO_GOAL_CURRENT = 2
 
 # Protocol version
-PROTOCOL_VERSION       = 2.0                   # Protocol version
+PROTOCOL_VERSION = 2.0                         # Protocol version
 
 # Default setting
-DXL_ID                 = 7                     # Dynamixel ID
-BAUDRATE               = 57600                 # Baudrate
-DEVICENAME = '/dev/DYNAMIXEL'                # Check which port is being used on your controller
+DXL_ID = 7                                     # Dynamixel ID
+BAUDRATE = 57600                               # Dynamixel default baudrate : 57600
+DEVICENAME = '/dev/DYNAMIXEL'                  # Check which port is being used on your controller
 
-TORQUE_ENABLE          = 1                     # Value for enabling the torque
-TORQUE_DISABLE         = 0                     # Value for disabling the torque
-CURRENT_CONTROL_MODE   = 0                     # Current control mode is 0x00
-
+TORQUE_ENABLE = 1                              # Value for enabling the torque
+TORQUE_DISABLE = 0                             # Value for disabling the torque
+CURRENT_CONTROL_MODE = 0                       # Current Control mode
+GOAL_CURRENT_VALUE = 10                        # Goal current in mA (10 mA)
 # Initialize PortHandler instance
 portHandler = PortHandler(DEVICENAME)
 
@@ -30,74 +28,51 @@ packetHandler = PacketHandler(PROTOCOL_VERSION)
 
 # Open port
 if portHandler.openPort():
-    print("Port opened successfully")
+    print("Succeeded to open the port")
 else:
     print("Failed to open the port")
-    exit()
+    quit()
 
 # Set port baudrate
 if portHandler.setBaudRate(BAUDRATE):
-    print("Baudrate set successfully")
+    print("Succeeded to change the baudrate")
 else:
     print("Failed to change the baudrate")
-    exit()
-
-# Set operating mode to current control mode
-dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_OPERATING_MODE, CURRENT_CONTROL_MODE)
+    quit()
+# Set operating mode to current-based control
+dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_PRO_OPERATING_MODE, CURRENT_CONTROL_MODE)
 if dxl_comm_result != COMM_SUCCESS:
     print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
 elif dxl_error != 0:
     print("%s" % packetHandler.getRxPacketError(dxl_error))
 else:
-    print("Operating mode set to current control mode")
+    print("Operating mode changed to current control mode.")
 
 # Enable Dynamixel Torque
-dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_TORQUE_ENABLE, TORQUE_ENABLE)
+dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_PRO_TORQUE_ENABLE, TORQUE_ENABLE)
 if dxl_comm_result != COMM_SUCCESS:
     print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
 elif dxl_error != 0:
     print("%s" % packetHandler.getRxPacketError(dxl_error))
 else:
-    print("Dynamixel has been successfully enabled")
-
-# Read Hardware Error Status
-dxl_comm_result, dxl_error, dxl_hw_error = packetHandler.read1ByteTxRx(portHandler, DXL_ID, ADDR_HARDWARE_ERROR)
-if dxl_comm_result != COMM_SUCCESS:
-    print("Failed to read hardware error status: %s" % packetHandler.getTxRxResult(dxl_comm_result))
-elif dxl_error != 0:
-    print("Communication error when reading hardware error status: %s" % packetHandler.getRxPacketError(dxl_error))
-else:
-    print("Hardware error status: %d" % dxl_hw_error)
-    if dxl_hw_error != 0:
-        print("Error Code: %d -> %s" % (dxl_hw_error, decode_hardware_error(dxl_hw_error)))
-
-def decode_hardware_error(error_code):
-    errors = []
-    if error_code & 1: errors.append("Input Voltage Error")
-    if error_code & 2: errors.append("Overheating Error")
-    if error_code & 4: errors.append("Motor Encoder Error")
-    if error_code & 8: errors.append("Electrical Shock Error")
-    if error_code & 16: errors.append("Overload Error")
-    return ", ".join(errors)
-
+    print("Dynamixel has been successfully enabled.")
 # Write goal current
-goal_current = 1000   # mA
-dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, DXL_ID, ADDR_GOAL_CURRENT, goal_current)
+dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, DXL_ID, ADDR_PRO_GOAL_CURRENT, GOAL_CURRENT_VALUE)
 if dxl_comm_result != COMM_SUCCESS:
     print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
 elif dxl_error != 0:
     print("%s" % packetHandler.getRxPacketError(dxl_error))
 else:
-    print("Goal current has been set")
+    print("Goal current has been set to %d mA" % GOAL_CURRENT_VALUE)
 
 # Disable Dynamixel Torque
-dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_TORQUE_ENABLE, TORQUE_DISABLE)
+dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_PRO_TORQUE_ENABLE, TORQUE_DISABLE)
 if dxl_comm_result != COMM_SUCCESS:
     print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
 elif dxl_error != 0:
     print("%s" % packetHandler.getRxPacketError(dxl_error))
 else:
-    print("Dynamixel has been successfully disabled")
+    print("Dynamixel has been successfully disabled.")
 
 # Close port
 portHandler.closePort()
