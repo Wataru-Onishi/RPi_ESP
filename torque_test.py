f01d906057eee4c5122f85f845b3cae571ec9cbc
Wataru Onishi <62089701+Wataru-Onishi@users.noreply.github.com>
2024-04-17 15:11:41 +0900

    Update torque_test.py

--- torque_test.py
+++ torque_test.py
@@ -1,87 +1,141 @@
-import dynamixel_sdk as dxl
+import os
+from dynamixel_sdk import *
 
 # Control table address
-ADDR_OPERATING_MODE = 11
-ADDR_CURRENT_LIMIT = 38
-ADDR_GOAL_POSITION = 30
-ADDR_PRESENT_POSITION = 36
+ADDR_PRO_GOAL_POSITION = 30
+ADDR_PRO_PRESENT_POSITION = 36
+ADDR_PRO_OPERATING_MODE = 11
+ADDR_PRO_TORQUE_ENABLE = 64
+ADDR_PRO_GOAL_CURRENT = 102
+ADDR_PRO_CURRENT_LIMIT = 38
+
+# Data Byte Length
+LEN_PRO_GOAL_POSITION = 4
+LEN_PRO_PRESENT_POSITION = 4
+LEN_PRO_OPERATING_MODE = 1
+LEN_PRO_TORQUE_ENABLE = 1
+LEN_PRO_GOAL_CURRENT = 2
+LEN_PRO_CURRENT_LIMIT = 2
 
 # Protocol version
 PROTOCOL_VERSION = 2.0
 
 # Default setting
-DXL_ID = 7  # DYNAMIXEL ID
-BAUDRATE = 57600  # Default baudrate
-DEVICENAME = '/dev/DYNAMIXEL'  # Default device name
+DXL_ID = 7
+BAUDRATE = 57600
+DEVICENAME = '/dev/DYNAMIXEL'
+
+# Control mode
+CURRENT_CONTROL_MODE = 0x0A
+POSITION_CONTROL_MODE = 3
+
+# Torque enable/disable
+TORQUE_ENABLE = 1
+TORQUE_DISABLE = 0
 
 # Initialize PortHandler instance
-portHandler = dxl.PortHandler(DEVICENAME)
+portHandler = PortHandler(DEVICENAME)
 
 # Initialize PacketHandler instance
-packetHandler = dxl.PacketHandler(PROTOCOL_VERSION)
+packetHandler = PacketHandler(PROTOCOL_VERSION)
 
 # Open port
 if portHandler.openPort():
     print("Succeeded to open the port")
 else:
     print("Failed to open the port")
-    quit()
+    os._exit(0)
 
-# Set baudrate
+# Set port baudrate
 if portHandler.setBaudRate(BAUDRATE):
     print("Succeeded to change the baudrate")
 else:
     print("Failed to change the baudrate")
-    quit()
+    os._exit(0)
 
-# Function to set operating mode to current control mode
-def set_current_control_mode():
-    dxl_comm_result, _ = packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_OPERATING_MODE, 0)
-    return dxl_comm_result == dxl.COMM_SUCCESS
+# Set operating mode to current control mode
+dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_PRO_OPERATING_MODE, CURRENT_CONTROL_MODE)
+if dxl_comm_result != COMM_SUCCESS:
+    print("Failed to change operating mode: %s" % packetHandler.getTxRxResult(dxl_comm_result))
+elif dxl_error != 0:
+    print("Error occurred while changing operating mode: %s" % packetHandler.getRxPacketError(dxl_error))
+else:
+    print("Operating mode set to current control.")
 
-# Function to set current limit
-def set_current_limit(current_limit):
-    dxl_comm_result, _ = packetHandler.write2ByteTxRx(portHandler, DXL_ID, ADDR_CURRENT_LIMIT, current_limit)
-    return dxl_comm_result == dxl.COMM_SUCCESS
-
-# Function to set goal position
-def set_goal_position(goal_position):
-    dxl_comm_result, _ = packetHandler.write4ByteTxRx(portHandler, DXL_ID, ADDR_GOAL_POSITION, goal_position)
-    return dxl_comm_result == dxl.COMM_SUCCESS
-
-# Function to get present position
-def get_present_position():
-    present_position, _, _ = packetHandler.read4ByteTxRx(portHandler, DXL_ID, ADDR_PRESENT_POSITION)
-    return present_position
-
-# Main code
+# Main loop
 try:
     while True:
-        command = input("Enter command (1: Set current control mode, 2: Move to position 1800, 3: Move to position 0, exit: Exit program): ")
-        
-        if command == '1':
-            if set_current_control_mode():
-                print("Changed to current control mode")
-                set_current_limit(6)  # Set current limit to 6mA
+        print("Type '1' to set current control mode and 6mA, '2' to move to position 1800, '3' to move to position 0, or 'exit' to quit:")
+        cmd = input()
+        if cmd == "1":
+            # Enable Dynamixel Torque
+            dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_PRO_TORQUE_ENABLE, TORQUE_ENABLE)
+            if dxl_comm_result != COMM_SUCCESS:
+                print("Failed to enable torque: %s" % packetHandler.getTxRxResult(dxl_comm_result))
+            elif dxl_error != 0:
+                print("Error occurred while enabling torque: %s" % packetHandler.getRxPacketError(dxl_error))
             else:
-                print("Failed to change to current control mode")
+                print("Torque enabled")
 
-        elif command == '2':
-            set_goal_position(1800)  # Move to position 1800
-            print("Moving to position 1800")
+            # Set goal current to 6mA
+            goal_current = 6
+            dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, DXL_ID, ADDR_PRO_GOAL_CURRENT, goal_current)
+            if dxl_comm_result != COMM_SUCCESS:
+                print("Failed to set goal current: %s" % packetHandler.getTxRxResult(dxl_comm_result))
+            elif dxl_error != 0:
+                print("Error occurred while setting goal current: %s" % packetHandler.getRxPacketError(dxl_error))
+            else:
+                print("Goal current set to %d mA" % goal_current)
 
-        elif command == '3':
-            set_goal_position(0)  # Move to position 0
-            print("Moving to position 0")
+        elif cmd == "2":
+            # Set operating mode to position control mode
+            dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_PRO_OPERATING_MODE, POSITION_CONTROL_MODE)
+            if dxl_comm_result != COMM_SUCCESS:
+                print("Failed to change operating mode: %s" % packetHandler.getTxRxResult(dxl_comm_result))
+            elif dxl_error != 0:
+                print("Error occurred while changing operating mode: %s" % packetHandler.getRxPacketError(dxl_error))
+            else:
+                print("Operating mode set to position control.")
 
-        elif command == 'exit':
+            # Move to position 1800
+            goal_position = 1800
+            dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, DXL_ID, ADDR_PRO_GOAL_POSITION, goal_position)
+            if dxl_comm_result != COMM_SUCCESS:
+                print("Failed to move to position: %s" % packetHandler.getTxRxResult(dxl_comm_result))
+            elif dxl_error != 0:
+                print("Error occurred while moving to position: %s" % packetHandler.getRxPacketError(dxl_error))
+            else:
+                print("Moving to position %d" % goal_position)
+
+        elif cmd == "3":
+            # Set operating mode to position control mode
+            dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_PRO_OPERATING_MODE, POSITION_CONTROL_MODE)
+            if dxl_comm_result != COMM_SUCCESS:
+                print("Failed to change operating mode: %s" % packetHandler.getTxRxResult(dxl_comm_result))
+            elif dxl_error != 0:
+                print("Error occurred while changing operating mode: %s" % packetHandler.getRxPacketError(dxl_error))
+            else:
+                print("Operating mode set to position control.")
+
+            # Move to position 0
+            goal_position = 0
+            dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, DXL_ID, ADDR_PRO_GOAL_POSITION, goal_position)
+            if dxl_comm_result != COMM_SUCCESS:
+                print("Failed to move to position: %s" % packetHandler.getTxRxResult(dxl_comm_result))
+            elif dxl_error != 0:
+                print("Error occurred while moving to position: %s" % packetHandler.getRxPacketError(dxl_error))
+            else:
+                print("Moving to position %d" % goal_position)
+
+        elif cmd == "exit":
+            print("Exiting...")
             break
 
         else:
-            print("Invalid command")
+            print("Invalid command!")
 
 except KeyboardInterrupt:
-    print("\nProgram interrupted")
+    print("Keyboard Interrupt. Exiting...")
 
 # Close port
 portHandler.closePort()
