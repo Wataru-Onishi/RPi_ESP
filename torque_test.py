a41f6d47d38d434526fbdb5ed48d0efc47d9ac36
Wataru Onishi <62089701+Wataru-Onishi@users.noreply.github.com>
2024-04-19 11:46:51 +0900

    Update torque_test.py

--- torque_test.py
+++ torque_test.py
@@ -23,72 +23,72 @@ DEVICENAME = '/dev/DYNAMIXEL'                  # Check which port is being used 
 TORQUE_ENABLE = 1                              # Value for enabling the torque
 TORQUE_DISABLE = 0                             # Value for disabling the torque
 
-# Current control mode and position control mode setting
+# Operating Modes
 CURRENT_CONTROL_MODE = 0                       # Current control mode
-POSITION_CONTROL_MODE = 3                      # Position control mode (extended position control mode might be 4)
+POSITION_CONTROL_MODE = 3                      # Position control mode
 
 # Initialize PortHandler instance
-# Set the port path
-# Get methods and members of PortHandlerLinux or PortHandlerWindows
 portHandler = PortHandler(DEVICENAME)
 
 # Initialize PacketHandler instance
 packetHandler = PacketHandler(PROTOCOL_VERSION)
 
 # Open port
-if portHandler.openPort():
-    print("Succeeded to open the port")
-else:
-    print("Failed to open the port")
-    print("Press any key to terminate...")
-    input()
+if not portHandler.openPort():
+    print("Failed to open the port!")
     quit()
 
 # Set port baudrate
-if portHandler.setBaudRate(BAUDRATE):
-    print("Succeeded to change the baudrate")
-else:
-    print("Failed to change the baudrate")
-    print("Press any key to terminate...")
-    input()
+if not portHandler.setBaudRate(BAUDRATE):
+    print("Failed to change the baudrate!")
     quit()
 
-# Enable Dynamixel Torque
-dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_TORQUE_ENABLE, TORQUE_ENABLE)
-if dxl_comm_result != COMM_SUCCESS:
-    print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
-elif dxl_error != 0:
-    print("%s" % packetHandler.getRxPacketError(dxl_error))
-else:
-    print("Dynamixel has been successfully connected")
+def enable_torque(enable):
+    return packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_TORQUE_ENABLE, enable)
 
-while True:
-    print("Press 1 to set 6mA current, 2 to move to position 1800, 3 to move to position 0, or type 'exit' to exit.")
-    data = input()
-    if data == '1':
-        # Set to current control mode
-        packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_OPERATING_MODE, CURRENT_CONTROL_MODE)
-        # Set goal current
-        packetHandler.write2ByteTxRx(portHandler, DXL_ID, ADDR_GOAL_CURRENT, 6)
-        print("Setting 6mA to Dynamixel")
-    elif data == '2':
-        # Set to position control mode
-        packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_OPERATING_MODE, POSITION_CONTROL_MODE)
-        # Move to position 1800
-        packetHandler.write4ByteTxRx(portHandler, DXL_ID, ADDR_GOAL_POSITION, 1800)
-        print("Moving to position 1800")
-    elif data == '3':
-        # Set to position control mode
-        packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_OPERATING_MODE, POSITION_CONTROL_MODE)
-        # Move to position 0
-        packetHandler.write4ByteTxRx(portHandler, DXL_ID, ADDR_GOAL_POSITION, 0)
-        print("Moving to position 0")
-    elif data == 'exit':
-        break
-    else:
-        print("Invalid input. Please try again.")
+def set_operating_mode(mode):
+    # Disable Torque before changing operating mode
+    enable_torque(TORQUE_DISABLE)
+    # Write new operating mode to motor
+    packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_OPERATING_MODE, mode)
+    # Re-enable Torque after changing operating mode
+    enable_torque(TORQUE_ENABLE)
 
-# Disable Dynamixel Torque
-packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_TORQUE_ENABLE, TORQUE_DISABLE)
-# Close port
-portHandler.closePort()
+def set_goal_current(current):
+    packetHandler.write2ByteTxRx(portHandler, DXL_ID, ADDR_GOAL_CURRENT, current)
+
+def set_goal_position(position):
+    packetHandler.write4ByteTxRx(portHandler, DXL_ID, ADDR_GOAL_POSITION, position)
+
+# Ensure Dynamixel has been successfully connected
+if enable_torque(TORQUE_ENABLE)[0] != COMM_SUCCESS:
+    print("Failed to enable torque!")
+    quit()
+
+print("Dynamixel has been successfully connected")
+
+try:
+    while True:
+        print("Press 1 to set 6mA current, 2 to move to position 1800, 3 to move to position 0, or type 'exit' to exit.")
+        data = input()
+        if data == '1':
+            set_operating_mode(CURRENT_CONTROL_MODE)
+            set_goal_current(6)
+            print("6mA current set.")
+        elif data == '2':
+            set_operating_mode(POSITION_CONTROL_MODE)
+            set_goal_position(1800)
+            print("Moving to position 1800.")
+        elif data == '3':
+            set_operating_mode(POSITION_CONTROL_MODE)
+            set_goal_position(0)
+            print("Moving to position 0.")
+        elif data == 'exit':
+            break
+        else:
+            print("Invalid input. Please try again.")
+finally:
+    # Disable Torque on exit
+    enable_torque(TORQUE_DISABLE)
+    # Close port
+    portHandler.closePort()
