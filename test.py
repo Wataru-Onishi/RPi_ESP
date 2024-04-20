1a1eeec6e510840eb26f011b4103ad1fd8650d73
Wataru Onishi <62089701+Wataru-Onishi@users.noreply.github.com>
2024-04-20 16:00:46 +0900

    Update test.py

--- test.py
+++ test.py
@@ -105,13 +105,13 @@ try:
                     set_operating_mode(DXL_ID_5, VELOCITY_CONTROL_MODE)
                     set_operating_mode(DXL_ID_6, VELOCITY_CONTROL_MODE)
                     set_goal_velocity(DXL_ID_5, goal_velocity_forward)
-                    set_goal_velocity(DXL_ID_6, goal_velocity_forward)
+                    set_goal_velocity(DXL_ID_6, -goal_velocity_forward)
                     print("Motors 5 and 6 are set to move forward at controlled speed.")
                 elif joystick.get_hat(0) == (0, -1):  # D-pad Down
                     set_operating_mode(DXL_ID_5, VELOCITY_CONTROL_MODE)
                     set_operating_mode(DXL_ID_6, VELOCITY_CONTROL_MODE)
                     set_goal_velocity(DXL_ID_5, goal_velocity_backward)
-                    set_goal_velocity(DXL_ID_6, goal_velocity_backward)
+                    set_goal_velocity(DXL_ID_6, -goal_velocity_backward)
                     print("Motors 5 and 6 are set to move backward at controlled speed.")
                 elif joystick.get_hat(0) == (1, 0):  # D-pad Right
                     set_operating_mode(DXL_ID_5, VELOCITY_CONTROL_MODE)
